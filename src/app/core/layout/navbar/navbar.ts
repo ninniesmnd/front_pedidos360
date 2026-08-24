ae0e34005bca11c723bd38fa831f4535aa994629
Nicole <nc.2012.nicole@gmail.com>
2026-08-24 10:31:28 -0400

    Feat/Agregar Log Out

--- src/app/core/layout/navbar/navbar.ts
+++ src/app/core/layout/navbar/navbar.ts
@@ -1,5 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { RouterLink } from '@angular/router';
+import { MsalBroadcastService, MsalService } from '@azure/msal-angular';
+import { InteractionStatus } from '@azure/msal-browser';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -8,9 +12,26 @@ import { RouterLink } from '@angular/router';
   styleUrl: './navbar.css'
 })
 export class Navbar {
+  private readonly msalService = inject(MsalService);
+  private readonly msalBroadcastService = inject(MsalBroadcastService);
+
   protected readonly menuAbierto = signal(false);
+
+  protected readonly estaAutenticado = toSignal(
+    this.msalBroadcastService.inProgress$.pipe(
+      filter((estado) => estado === InteractionStatus.None),
+      map(() => this.msalService.instance.getAllAccounts().length > 0)
+    ),
+    { initialValue: this.msalService.instance.getAllAccounts().length > 0 }
+  );
 
   protected alternarMenu(): void {
     this.menuAbierto.update(valor => !valor);
+  }
+
+  protected cerrarSesion(): void {
+    this.msalService.logoutRedirect({
+      postLogoutRedirectUri: '/'
+    });
   }
 }
