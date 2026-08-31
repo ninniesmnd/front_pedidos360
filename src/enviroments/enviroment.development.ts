173fa6486841c8a201653a5bf107c77cc41f615a
Nicole <nc.2012.nicole@gmail.com>
2026-08-31 09:53:33 -0400

    Feat/ Migracion-nube

--- src/enviroments/enviroment.development.ts
+++ src/enviroments/enviroment.development.ts
@@ -3,6 +3,6 @@ export const environment = {
   msal: {
     clientId: '16ad8d4e-5e08-4308-bf95-7f1d30c55b4b',
     tenantId: '1b185538-3c99-4453-8c77-b86e78fa3157',
-    redirectUri: 'http://localhost:4200'
+    redirectUri: 'https://52.71.213.234'
   }
 };
