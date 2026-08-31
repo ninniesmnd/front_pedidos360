ee1b8e43bef30afd477a71ae4dabfbcdb35dce43
Nicole <nc.2012.nicole@gmail.com>
2026-08-31 11:01:30 -0400

    Feat/enviroments
    * Fix/nombreUsuario-dashboard
    
    * Feat/enviroment-prod-desarrollo

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,5 +1,7 @@
+//PRODUCCION
+
 export const environment = {
-  production: false,
+  production: true,
   msal: {
     clientId: '16ad8d4e-5e08-4308-bf95-7f1d30c55b4b',
     tenantId: '1b185538-3c99-4453-8c77-b86e78fa3157',
